00577677156cd9394ea2a32028f684cbce178065
wkgcass <wkgcass@hotmail.com>
2021-03-15 19:46:50 +0800

    1.2.3 release

--- src/main/java/vjson/util/VERSION.java
+++ src/main/java/vjson/util/VERSION.java
@@ -13,7 +13,7 @@
 package vjson.util;
 
 public class VERSION {
-    public static final String VERSION = "1.2.2"; // _THE_VERSION_
+    public static final String VERSION = "1.2.3"; // _THE_VERSION_
 
     public VERSION() {
     }
