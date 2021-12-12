fee3a86bdc6bc56614e9881c08a5438fc99ae281
wkgcass <wkgcass@hotmail.com>
2021-12-12 19:00:28 +0800

    upgrade sample programs in web interpreter

--- misc/online_vjson_lang_interpreter/src/main/kotlin/Main.kt
+++ misc/online_vjson_lang_interpreter/src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
+import vjson.CharStream
+import vjson.cs.LineColCharStream
+import vjson.ex.JsonParseException
 import vjson.ex.ParserException
+import vjson.parser.ObjectParser
+import vjson.pl.ASTGen
 import vjson.pl.InterpreterBuilder
 import vjson.pl.type.lang.StdTypes
 
@@ -23,7 +28,7 @@ fun registerCursorJump(func: (Int, Int) -> Unit) {
 
 @ExperimentalJsExport
 @JsExport
-fun run(prog: String) {
+fun run(prog: String, printMem: Boolean) {
   val stdTypes = StdTypes()
   stdTypes.setOutput(outputFunc)
   val builder = InterpreterBuilder()
@@ -31,20 +36,64 @@ fun run(prog: String) {
   val interpreter = try {
     builder.compile(prog)
   } catch (e: Throwable) {
-    outputFunc("Compilation failed")
-    outputFunc(e.toString())
-    if (e is ParserException) {
-      val lineCol = e.lineCol
-      if (lineCol != null) {
-        cursorJumpFunc(lineCol.line, lineCol.col)
-      }
-    }
+    printParsingFailedMessage(e, "Compilation failed")
     return
   }
-  try {
+  val mem = try {
     interpreter.execute()
   } catch (e: Throwable) {
     outputFunc("Runtime failure")
-    outputFunc(e.toString())
+    outputFunc(e.message ?: "")
+    return
   }
+  if (printMem) {
+    outputFunc(mem.toString())
+  }
+}
+
+fun printParsingFailedMessage(e: Throwable, msg: String = "Parsing failed") {
+  outputFunc(msg)
+  outputFunc(e.message ?: "")
+  if (e is ParserException) {
+    val lineCol = e.lineCol
+    if (lineCol != null) {
+      cursorJumpFunc(lineCol.line, lineCol.col)
+    }
+  }
+}
+
+fun getJson(prog: String): vjson.JSON.Object? {
+  val parser = ObjectParser(InterpreterBuilder.interpreterOptions())
+  val obj = try {
+    parser.last(LineColCharStream(CharStream.from(prog), ""))
+  } catch (e: JsonParseException) {
+    printParsingFailedMessage(e)
+    return null
+  }
+  if (obj == null) { // should not happen, but we check it anyway
+    outputFunc("Parsing failed, no output generated")
+    return null
+  }
+  return obj
+}
+
+@ExperimentalJsExport
+@JsExport
+fun json(prog: String) {
+  val obj = getJson(prog) ?: return
+  outputFunc(obj.pretty())
+}
+
+@ExperimentalJsExport
+@JsExport
+fun ast(prog: String) {
+  val obj = getJson(prog) ?: return
+  val astGen = ASTGen(obj)
+  val ast = try {
+    astGen.parse()
+  } catch (e: ParserException) {
+    printParsingFailedMessage(e)
+    return
+  }
+  outputFunc(ast.toString())
 }
