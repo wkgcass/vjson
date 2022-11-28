fa363cb8a31e96181048d5cc688ebc39f1fab1d5
wkgcass <wkgcass@hotmail.com>
2022-11-28 14:22:09 +0800

    update version to 1.4.1-dev

--- src/main/kotlin/vjson/util/VERSION.kt
+++ src/main/kotlin/vjson/util/VERSION.kt
@@ -13,5 +13,5 @@ package vjson.util
 
 // #ifdef COVERAGE {{@lombok.Generated}}
 object VERSION {
-  const val VERSION = "1.4.0" // _THE_VERSION_
+  const val VERSION = "1.4.1-dev" // _THE_VERSION_
 }
